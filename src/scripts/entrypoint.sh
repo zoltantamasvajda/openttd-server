c75d8c66c6fb7e8d589638f37adf5648339a62d5
Zoltan Tamas Vajda <zoltan.tamas.vajda@gmail.com>
2021-05-14 13:34:22 +0200

    Generate new savegame if autosave is not found instead of failing to start

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -8,38 +8,40 @@ loadgame="$(snapctl get loadgame)"
 echo ${loadgame}
 
 if [[ -v loadgame ]]; then
-        case "${loadgame}" in
-                'false')
-                        echo "Creating a new game."
-                        exec ${SNAP}/usr/games/openttd/openttd -D -x 
-                        exit 0
-                ;;
-                'last-autosave')
-            		savegame_target=${savepath}/autosave/`ls -rt ${savepath}/autosave/ | tail -n1`
-
-            		if [ -r ${savegame_target} ]; then
-                    	echo "Loading from autosave - ${savegame_target}"
-                        exec ${SNAP}/usr/games/openttd/openttd -D -g ${savegame_target} -x 
-                        exit 0
-            		else
-                		echo "${savegame_target} not found..."
-                		exit 1
-            		fi
-                ;;
-                *)
-                	savegame_target="${savepath}/${loadgame}"
-                    if [ -f ${savegame_target} ]; then
-                            echo "Loading ${savegame_target}"
-                            exec ${SNAP}/usr/games/openttd/openttd -D -g ${savegame_target} -x 
-                            exit 0
-                    else
-                            echo "${savegame_target} not found..."
-                            exit 1
-                    fi
-                ;;
-        esac
-else
-        echo "loadgame not set - Creating a new game."
-    	exec ${SNAP}/usr/games/openttd/openttd -D -x 
+    case "${loadgame}" in
+    'false')
+        echo "Creating a new game."
+        exec ${SNAP}/usr/games/openttd/openttd -D -x
         exit 0
+        ;;
+    'last-autosave')
+        savegame_target=${savepath}/autosave/$(ls -rt ${savepath}/autosave/ | tail -n1)
+
+        if [ -r ${savegame_target} ]; then
+            echo "Loading from autosave - ${savegame_target}"
+            exec ${SNAP}/usr/games/openttd/openttd -D -g ${savegame_target} -x
+            exit 0
+        else
+            echo "${savegame_target} not found..."
+            exec ${SNAP}/usr/games/openttd/openttd -D -x
+            exit 0
+        fi
+        ;;
+    *)
+        savegame_target="${savepath}/${loadgame}"
+        if [ -f ${savegame_target} ]; then
+            echo "Loading ${savegame_target}"
+            exec ${SNAP}/usr/games/openttd/openttd -D -g ${savegame_target} -x
+            exit 0
+        else
+            echo "${savegame_target} not found..."
+            exec ${SNAP}/usr/games/openttd/openttd -D -x
+            exit 0
+        fi
+        ;;
+    esac
+else
+    echo "loadgame not set - Creating a new game."
+    exec ${SNAP}/usr/games/openttd/openttd -D -x
+    exit 0
 fi
